11980a93701e8dc6cbd2dd86fa5a1eb3a38a6994
tawhid <ahmedshanto690@gmail.com>
2025-12-31 20:50:23 +0600

    first commit

---
+++ src/server.ts
@@ -0,0 +1,63 @@
+import express, { Request, Response } from 'express';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+const app = express();
+const port = 5000;
+// parser
+app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+// database connection
+const pool = new Pool({
+  connectionString: `${process.env.CONECTION_STR}`,
+});
+
+const initDB = async () => {
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS users (
+     id SERIAL PRIMARY KEY,
+     name VARCHAR(100) NOT NULL,
+     email VARCHAR(100) NOT NULL UNIQUE,
+     age INT, 
+     phone VARCHAR(15), 
+     address TEXT, 
+      created_at TIMESTAMP DEFAULT NOW(),
+      updated_at TIMESTAMP DEFAULT NOW()
+  )
+`);
+
+  await pool.query(`
+    CREATE TABLE IF NOT EXISTS todos  (
+    id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES users(id) ON DELETE CASCADE,
+    titel VARCHAR(200) NOT NULL, 
+    description TEXT, 
+    completed BOOLEAN DEFAULT false, 
+    due_date DATE, 
+    created_at TIMESTAMP DEFAULT NOW(), 
+    updated_at TIMESTAMP DEFAULT NOW () 
+    )
+    
+    
+    `);
+};
+
+initDB();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello World! tawhid here');
+});
+
+app.post('/', (req: Request, res: Response) => {
+  console.log(req.body);
+  res.status(201).json({
+    succcess: true,
+    message: 'Data received successfully',
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});

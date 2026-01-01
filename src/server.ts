a6f8d9211d3e5bbcf2a0ea01c9585f013a7c89ed
tawhid <ahmedshanto690@gmail.com>
2026-01-01 21:25:18 +0600

    feat: add CRUD operations for users with error handling

--- src/server.ts
+++ src/server.ts
@@ -91,6 +91,110 @@ app.get('/users', async (req: Request, res: Response) => {
   }
 });
 
+// Get a single user by ID
+app.get('/users/:id', async (req: Request, res: Response) => {
+  // console.log(req.params.id);
+  // res.send({
+  //   message: 'API is cool',
+  // });
+
+  try {
+    const result = await pool.query(`SELECT * FROM users WHERE id = $1`, [
+      req.params.id,
+    ]);
+    console.log(result.rows);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: 'user not found',
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: 'user succsesfully',
+        data: result.rows[0],
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// update user single by id
+
+app.put('/users/:id', async (req: Request, res: Response) => {
+  // console.log(req.params.id);
+  // res.send({
+  //   message: 'API is cool',
+  // });
+
+  const { name, email } = req.body;
+
+  try {
+    const result = await pool.query(
+      `UPDATE users SET name=$1, email=$2 WHERE id=$3 RETURNING * `,
+      [name, email, req.params.id]
+    );
+    console.log(result.rows);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: 'user not found',
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: 'user update succsesfully',
+        data: result.rows[0],
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
+// delet user
+
+app.delete('/users/:id', async (req: Request, res: Response) => {
+  // console.log(req.params.id);
+  // res.send({
+  //   message: 'API is cool',
+  // });
+
+  try {
+    const result = await pool.query(`DELETE * FROM users WHERE id = $1`, [
+      req.params.id,
+    ]);
+    console.log(result.rows);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: 'user not found',
+      });
+    } else {
+      res.status(200).json({
+        success: true,
+        message: 'user DELETE succsesfully',
+        data: null,
+      });
+    }
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.message,
+    });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
